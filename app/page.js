ae278a97daa4693061f35602cf95c73a88f51513
banuka <banukaramanayaka76@gmail.com>
2024-03-11 22:39:44 +0530

    feat:header

--- app/page.js
+++ app/page.js
@@ -1,18 +1,12 @@
+import Header from "@/components/Header";
 import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <>
       <Wrapper>
-        <div>
-          <h1>fashion bug</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate in nihil eligendi dolor reprehenderit et modi magnam vero corporis. Blanditiis commodi numquam hic. Enim, excepturi accusantium eaque in quidem rerum?
-            Harum itaque excepturi omnis. Omnis harum placeat minus velit? Ipsa voluptas fugit labore deserunt adipisci veritatis ea, temporibus nisi dolorem iusto, perferendis nostrum culpa? Illum minus ullam fuga est commodi.
-            Sapiente iusto nostrum unde labore accusamus dolore magnam officiis odit praesentium totam. Esse minus ducimus sapiente fuga, tenetur eaque facilis fugit vitae, deserunt animi dignissimos excepturi impedit repellat sunt blanditiis.
-            Excepturi aliquid, eaque sequi minus error facere porro officiis repellat voluptatibus fugit, provident necessitatibus, ipsa explicabo modi distinctio numquam alias quia cumque nemo veritatis fugiat perferendis expedita assumenda. Molestias, nobis!
-            Molestiae quas recusandae dolorem architecto, dignissimos officiis, magni quaerat impedit voluptas adipisci, quam iusto! Tenetur, dignissimos? Architecto exercitationem alias accusantium incidunt maxime quas saepe mollitia placeat. Voluptatem harum nulla cum.
-            Nihil voluptates ut minima modi, </p>
-        </div>
+        <Header />
+        <div className="w-full bg-red-300 h-96"></div>
       </Wrapper>
 
     </>
