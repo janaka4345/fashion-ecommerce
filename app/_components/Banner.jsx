f534a8f0696c9c02a4a3485e01bae14af07c2b6f
banuka <banukaramanayaka76@gmail.com>
2024-03-14 23:04:32 +0530

    style:responsive

--- app/_components/Banner.jsx
+++ app/_components/Banner.jsx
@@ -8,9 +8,9 @@ import {
 } from "@/components/ui/carousel";
 export default function Banner() {
   return (
-    <section className="flex  w-full flex-row gap-4 bg-green-200 ">
-      <div className="w-1/2">hi</div>
-      <div className="mx-8 w-1/2">
+    <section className="flex  w-full  flex-col gap-4 bg-green-200 sm:flex-row ">
+      <div className="min-h-28 sm:w-1/2">hi</div>
+      <div className="mx-8 sm:w-1/2">
         <Carousel className=" w-full">
           <CarouselContent className="">
             {Array.from({ length: 5 }).map((_, index) => (
