2017c1cf0bb9d2b33c1a5ec4be9e8667418c113d
banuka <banukaramanayaka76@gmail.com>
2024-03-15 23:08:01 +0530

    style:responsive price card

--- components/PriceCard.jsx
+++ components/PriceCard.jsx
@@ -11,12 +11,13 @@ import { cn } from "@/lib/utils";
 import { Icon } from "./Icons";
 
 export default function PriceCard() {
+  const rating = 2;
   return (
-    <Card className=" w-[16svw] min-w-[14svw] max-w-[18svw] rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+    <Card className="relative w-[16svw] min-w-fit max-w-[18svw] rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
       <CardHeader className="p-0 pt-3">
         <Link href="#">
           <Image
-            className="mx-auto rounded-t-lg"
+            className="mx-auto h-[75px] w-[75px] rounded-t-lg object-contain sm:h-[150px] sm:w-[150px]"
             width={150}
             height={150}
             src="/testApple/1.jpg"
@@ -26,83 +27,43 @@ export default function PriceCard() {
         {/* <CardTitle>Card Title</CardTitle>
         <CardDescription>Card Description</CardDescription> */}
       </CardHeader>
-      <CardContent className="mx-auto p-0 px-5 pt-3">
+      <CardContent className="mx-auto p-0 px-2 pt-3 sm:px-5">
         <Link href="#">
-          <span className="text-2xl font-bold text-gray-900 dark:text-white">
+          <span className="text-lg font-bold text-gray-900 dark:text-white sm:text-2xl">
             $599
           </span>
-          <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="text-sm font-semibold tracking-tight text-gray-900 dark:text-white sm:text-base">
             Apple Watch Series
           </h5>
         </Link>
         <div className="mb-5 mt-2.5 flex items-center">
           <div className="flex items-center space-x-1 rtl:space-x-reverse">
-            {/* <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg> */}
-            <Icon name={"star"} className="h-5 w-5 fill-yellow-300" />
-            <Icon name={"star"} className="h-5 w-5   fill-yellow-300" />
-            <Icon name={"star"} className="h-5 w-5   fill-yellow-300" />
-            <Icon name={"star"} className="h-5 w-5   fill-yellow-300" />
-            <Icon name={"star"} className="ck h-5  w-5 fill-gray-400" />
-            {/* <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-gray-200 dark:text-gray-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg> */}
+            {[...Array(5)].map((_, i) => (
+              <Icon
+                key={i}
+                name={"star"}
+                className={cn("h-4 w-4  sm:h-5 sm:w-5", {
+                  "fill-yellow-300": i + 1 <= rating,
+                  "fill-gray-400": i + 1 > rating,
+                })}
+              />
+            ))}
           </div>
           <span className="ms-3 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800">
-            5.0
+            {rating}
           </span>
         </div>
       </CardContent>
-      <CardFooter className="flex  flex-col items-center  gap-2 ">
+      <CardFooter className="flex  flex-col items-center  gap-2 p-3 sm:p-6 ">
         <Link
           href="#"
-          className={cn(buttonVariants({ variant: "default" }), "w-full")}
+          className={cn(buttonVariants({ variant: "default" }), "w-full ")}
           // className="w-full rounded-lg bg-blue-700 px-2 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Add to cart
         </Link>
         <Link
-          className={cn(buttonVariants({ variant: "outline" }), "w-full")}
+          className={cn(buttonVariants({ variant: "outline" }), "w-full ")}
           href="#"
           // className="w-full rounded-lg bg-blue-700 px-2 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
