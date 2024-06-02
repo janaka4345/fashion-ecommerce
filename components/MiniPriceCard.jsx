825b0f30ef0b4c88a9bb502e36f5fb36f58dfdd5
Janaka dev <janakchamantha12@gmail.com>
2024-06-02 22:26:01 +0530

    style:fix inline svg styles

--- components/MiniPriceCard.jsx
+++ components/MiniPriceCard.jsx
@@ -8,10 +8,9 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { Icon } from "./Icons";
 
 export default function MiniPriceCard() {
-  const rating = 2;
+  const rating = 3;
   return (
     <Card className="relative flex min-h-fit max-w-fit flex-row rounded-lg border border-gray-200 bg-white pb-1 pl-1 pt-1 shadow dark:border-gray-700 dark:bg-gray-800 sm:w-[300px] sm:pb-0 ">
       <CardHeader className="h-full  w-full bg-purple-400 p-0 py-0 ">
@@ -39,14 +38,23 @@ export default function MiniPriceCard() {
         <div className="mb-1 mt-1 flex items-center ">
           <div className="flex items-center justify-start sm:space-x-1">
             {[...Array(5)].map((_, i) => (
-              <Icon
+              <svg
                 key={i}
-                name={"star"}
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className={cn("h-3 w-3  sm:h-5 sm:w-5", {
-                  "fill-yellow-300": i + 1 <= rating,
-                  "fill-gray-400": i + 1 > rating,
+                  "fill-yellow-300 dark:stroke-black": i + 1 <= rating,
+                  "fill-gray-100 dark:stroke-black": i + 1 > rating,
                 })}
-              />
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
             ))}
           </div>
           <span className="ms-3 rounded bg-blue-100 px-1.5 py-0.5 text-xs  font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800 sm:px-2.5">
--- components/PriceCard.jsx
+++ components/PriceCard.jsx
@@ -9,7 +9,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
-import { Icon } from "./Icons";
 
 export default function PriceCard() {
   const rating = 2;
@@ -40,14 +39,23 @@ export default function PriceCard() {
         <div className="mb-1 mt-1 flex items-center sm:mb-5 sm:mt-2.5">
           <div className="flex items-center justify-start sm:space-x-1">
             {[...Array(5)].map((_, i) => (
-              <Icon
+              <svg
                 key={i}
-                name={"star"}
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className={cn("h-3 w-3  sm:h-5 sm:w-5", {
-                  "fill-yellow-300": i + 1 <= rating,
-                  "fill-gray-400": i + 1 > rating,
+                  "fill-yellow-300 dark:stroke-black": i + 1 <= rating,
+                  "fill-gray-100 dark:stroke-black": i + 1 > rating,
                 })}
-              />
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
             ))}
           </div>
           <span className="ms-3 rounded bg-blue-100 px-1.5 py-0.5 text-xs  font-semibold text-blue-800 dark:bg-blue-200 dark:text-blue-800 sm:px-2.5">
