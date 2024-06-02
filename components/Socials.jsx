81f66e447f6ad4a6e89763afa011918dbed655d6
Janaka dev <janakchamantha12@gmail.com>
2024-06-02 22:18:42 +0530

    other:fix inline svg style issues

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
-import { Icon } from "@/components/Icons";
 
 export default function Socials() {
   return (
@@ -11,8 +10,7 @@ export default function Socials() {
           className={buttonVariants({ variant: "link", size: "icon" })}
           href={item.href}
         >
-          <Icon className="h-6 w-6" name={item.icon} />
-          {/* {item.name} */}
+          {item.icon}
         </Link>
       ))}
     </div>
@@ -22,17 +20,60 @@ export default function Socials() {
 const socialData = [
   {
     name: "Facebook",
-    href: "/",
-    icon: "facebook",
+    href: "https://fb.com/",
+    icon: (
+      <>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+        </svg>
+      </>
+    ),
   },
   {
     name: "Instagram",
     href: "/",
-    icon: "instagram",
+    icon: (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+      </svg>
+    ),
   },
   {
     name: "x",
-    href: "/",
-    icon: "twitter",
+    href: "https://x.com/",
+    icon: (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+      </svg>
+    ),
   },
 ];
