f9d4101c5d091afd4d249c7523e9c579e813f593
Janaka dev <janakchamantha12@gmail.com>
2024-06-02 20:40:56 +0530

    other:sly icon component removed

--- app/_components/Categories.jsx
+++ app/_components/Categories.jsx
@@ -7,37 +7,41 @@ export default function Categories() {
       <div className="group  relative col-start-1 col-end-3  row-start-1 row-end-2 flex flex-col items-center justify-center overflow-hidden    sm:col-start-1 sm:col-end-3 sm:row-start-1 sm:row-end-2">
         {/* TODO lint images */}
         <Image
+          alt="iamge"
           className="absolute inset-0 -z-50 object-cover duration-300 group-hover:scale-125"
           src="/cat1.jpg"
           fill
-        ></Image>
+        />
         <h1 className="text-lg text-white">Leather Belt</h1>
         <Button>Shop Now</Button>
       </div>
       <div className="group relative col-start-1 col-end-2 row-start-2 row-end-3  flex flex-col items-center justify-center overflow-hidden  sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-2">
         <Image
+          alt="iamge"
           className="absolute inset-0 -z-50 object-cover duration-300 group-hover:scale-125"
           src="/cat2.jpg"
           fill
-        ></Image>
+        />
         <h1 className="text-lg text-white">Leather Belt</h1>
         <Button>Shop Now</Button>
       </div>
       <div className="group relative col-start-2 col-end-3 row-start-2 row-end-3  flex flex-col items-center justify-center overflow-hidden  sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-3">
         <Image
+          alt="iamge"
           className="absolute inset-0 -z-50 object-cover duration-300 group-hover:scale-125"
           src="/cat3.jpg"
           fill
-        ></Image>
+        />
         <h1 className="text-lg text-white">Leather Belt</h1>
         <Button>Shop Now</Button>
       </div>
       <div className="group relative col-start-1 col-end-3 row-start-3 row-end-4  flex flex-col items-center justify-center overflow-hidden  sm:col-start-2 sm:col-end-4 sm:row-start-2 sm:row-end-3">
         <Image
+          alt="iamge"
           className="absolute inset-0 -z-50 object-cover duration-300 group-hover:scale-125"
           src="/cat4.jpg"
           fill
-        ></Image>
+        />
         <h1 className="text-lg text-white">Leather Belt</h1>
         <Button>Shop Now</Button>
       </div>
