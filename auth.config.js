c883508215850256cc0b3325c55632a60f47893a
Janaka dev <janakchamantha12@gmail.com>
2024-06-06 16:13:02 +0530

    feat:auth session extend for user role and id

--- auth.config.js
+++ auth.config.js
@@ -1,6 +1,7 @@
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
+import { getUserByEmail } from "./serverActions/users/getUsers"
 
 export default {
     providers: [Credentials({
@@ -12,7 +13,9 @@ export default {
         // },
         authorize: async (credentials) => {
             console.log(credentials);
-            let user = null
+            const existingUser = await getUserByEmail(credentials.email)
+
+
 
             // logic to salt and hash password
             // const pwHash = saltAndHashPassword(credentials.password)
@@ -20,14 +23,17 @@ export default {
             // logic to verify if user exists
             // user = await getUserFromDb(credentials.email, pwHash)
 
-            if (!user) {
+            if (!existingUser) {
                 // No user found, so this is their first attempt to login
                 // meaning this is also the place you could do registration
                 throw new Error("User not found.")
             }
+            if (existingUser.password != credentials.password) {
+                throw new Error("Invalid details")
+            }
 
             // return user object with the their profile data
-            return user
+            return existingUser
         },
     }),
     GitHub({
@@ -38,4 +44,5 @@ export default {
         clientId: process.env.AUTH_GOOGLE_ID,
         clientSecret: process.env.AUTH_GOOGLE_SECRET
     })]
+
 }
--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from './utils/prismaClient'
 import authConfig from "./auth.config"
+import { getUserById } from "./serverActions/users/getUsers"
 
 export const {
     auth,
@@ -11,5 +12,29 @@ export const {
 } = NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (!token.sub) {
+                return token
+            }
+
+            const existingUser = await getUserById(token.sub)
+            if (!existingUser) {
+                return token
+            }
+
+            token.role = existingUser.role
+
+            return token
+        },
+        async session({ session, token }) {
+
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+                session.user.role = token.role
+            }
+            return session
+        },
+    },
     ...authConfig,
 })
