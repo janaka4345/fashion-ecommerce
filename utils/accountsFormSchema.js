803b20602a02faf13a12fb78f7d8015f58e472d8
Janaka dev <janakchamantha12@gmail.com>
2024-06-07 19:31:45 +0530

    other:zod schema for products form

--- utils/accountsFormSchema.js
+++ utils/accountsFormSchema.js
@@ -3,4 +3,12 @@ export const startSellingFormSchema = z.object({
     shopName: z.string().trim().min(2, "Shop name must be at least 2 characters long").max(15, "Name must be less than 15 characters"),
     description: z.string().trim().min(2, "Shop description must be at least 2 characters long").max(30, "Name must be less than 30 characters long"),
     logo: z.string().optional(),
+})
+
+export const createProductFormSchema = z.object({
+    name: z.string().trim().min(2, "Shop name must be at least 2 characters long").max(15, "Name must be less than 15 characters"),
+    description: z.string().trim().min(2, "Shop description must be at least 2 characters long").max(30, "Name must be less than 30 characters long"),
+    // images: z.array(z.string()),
+    images: z.string(),
+    categoryId: z.string()
 }) 
