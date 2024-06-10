7bae0b791d2d9a3f3291731d1b5a3f70b19991ec
Janaka dev <janakchamantha12@gmail.com>
2024-06-10 22:37:53 +0530

    fix: create product Schema updated to fix the FileList not defined error

--- utils/accountsFormSchema.js
+++ utils/accountsFormSchema.js
@@ -1,4 +1,12 @@
 import z from 'zod'
+
+const MAX_FILE_SIZE = 2000000
+const FILE_TYPES = [
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+    'image/webp',
+]
 export const startSellingFormSchema = z.object({
     shopName: z.string().trim().min(2, "Shop name must be at least 2 characters long").max(15, "Name must be less than 15 characters"),
     description: z.string().trim().min(2, "Shop description must be at least 2 characters long").max(30, "Name must be less than 30 characters long"),
@@ -9,6 +17,20 @@ export const createProductFormSchema = z.object({
     name: z.string().trim().min(2, "Shop name must be at least 2 characters long").max(15, "Name must be less than 15 characters"),
     description: z.string().trim().min(2, "Shop description must be at least 2 characters long").max(30, "Name must be less than 30 characters long"),
     // images: z.array(z.string()),
-    images: z.string(),
+    images:
+        typeof window === 'undefined' ?
+            z.any()
+                .refine((file) => file?.length >= 1, 'File is required.')
+                // .refine((files) => files.length >= 1 ? FILE_TYPES.includes(files?.values?.[0]?.type) ? true : false : true, 'Invalid file. choose either JPEG or PNG image')
+                // .refine((files) => files.length >= 1 ? files[0]?.size <= MAX_FILE_SIZE ? true : false : true, 'Max file size allowed is 8MB.')
+                .optional()
+            :
+            z.instanceof(FileList)
+                .refine((file) => file?.length >= 1, 'File is required.')
+                .refine((files) => files.length >= 1 ? FILE_TYPES.includes(files?.[0]?.type) ? true : false : true, 'Invalid file. choose either JPEG or PNG image')
+                .refine((files) => files.length >= 1 ? files[0]?.size <= MAX_FILE_SIZE ? true : false : true, 'Max file size allowed is 8MB.')
+                .optional()
+
+    ,
     categoryId: z.string()
 }) 
